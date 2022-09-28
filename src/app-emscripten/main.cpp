73fcbcb6721f4e3e1b2d49bd164ca91077976c44
Tobias Hienzsch <post@tobias-hienzsch.de>
2022-09-29 00:47:10 +0200

    [emscripten] Fix imgui binding includes

--- src/app-emscripten/main.cpp
+++ src/app-emscripten/main.cpp
@@ -1,8 +1,8 @@
 #include "imgui.h"
 //
-#include "imgui_impl_sdl.h"
+#include "bindings/imgui_impl_sdl.h"
 //
-#include "imgui_impl_opengl3.h"
+#include "bindings/imgui_impl_opengl3.h"
 //
 #include <stdio.h>
 //
