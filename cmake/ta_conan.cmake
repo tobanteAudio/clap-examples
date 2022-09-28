a75b4506cb5b61bd79d9c6430b527692dec329cf
Tobias Hienzsch <post@tobias-hienzsch.de>
2022-09-28 21:41:02 +0200

    Cleanup plugin CMake config

---
+++ cmake/ta_conan.cmake
@@ -0,0 +1,24 @@
+if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
+message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
+file(
+    DOWNLOAD
+        "https://raw.githubusercontent.com/conan-io/cmake-conan/0.18.1/conan.cmake"
+        "${CMAKE_BINARY_DIR}/conan.cmake"
+    TLS_VERIFY
+        ON
+)
+endif()
+
+include(${CMAKE_BINARY_DIR}/conan.cmake)
+conan_add_remote(NAME conancenter INDEX 0 URL https://center.conan.io)
+conan_cmake_autodetect(settings)
+conan_cmake_install(
+    CONANFILE
+        conanfile.txt
+    PATH_OR_REFERENCE
+        ${CMAKE_CURRENT_SOURCE_DIR}
+    BUILD
+        missing
+    SETTINGS
+        ${settings}
+)
