ae430adef9197e4ebd26061496538dc4082c1d8d
Tobias Hienzsch <post@tobias-hienzsch.de>
2022-10-12 18:51:21 +0200

    [emscripten] Remove global state

--- example/app-emscripten/main.cpp
+++ example/app-emscripten/main.cpp
@@ -10,12 +10,15 @@
 
 #include <emscripten.h>
 
-// Emscripten requires to have full control over the main loop. We're going to
-// store our SDL book-keeping variables globally. Having a single function that
-// acts as a loop prevents us to store state in the stack of said function. So
-// we need some location for this.
-SDL_Window* gWindow            = nullptr;
-SDL_GLContext gGraphicsContext = nullptr;
+struct Application
+{
+    SDL_Window* window            = nullptr;
+    SDL_GLContext graphicsContext = nullptr;
+
+    bool showDemoWindow = true;
+    ImVec4 color        = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    int counter         = 0;
+};
 
 // For clarity, our main loop code is declared at the end.
 static auto mainLoop(void*) -> void;
@@ -46,7 +49,8 @@ int main(int, char**)
     SDL_GetCurrentDisplayMode(0, &current);
     auto flags = SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI;
 
-    gWindow = SDL_CreateWindow(
+    auto app   = Application{};
+    app.window = SDL_CreateWindow(
         "Title",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
@@ -55,8 +59,8 @@ int main(int, char**)
         (SDL_WindowFlags)flags
     );
 
-    gGraphicsContext = SDL_GL_CreateContext(gWindow);
-    if (!gGraphicsContext) {
+    app.graphicsContext = SDL_GL_CreateContext(app.window);
+    if (!app.graphicsContext) {
         mc::print(stderr, "Failed to initialize WebGL context!\n");
         return 1;
     }
@@ -70,82 +74,47 @@ int main(int, char**)
     // attempt to do a fopen() of the imgui.ini file. You may manually call
     // LoadIniSettingsFromMemory() to load settings from your own storage.
     ImGui::GetIO().IniFilename = nullptr;
-
     ImGui::StyleColorsDark();
 
-    // Setup Platform/Renderer backends
-    ImGui_ImplSDL2_InitForOpenGL(gWindow, gGraphicsContext);
+    ImGui_ImplSDL2_InitForOpenGL(app.window, app.graphicsContext);
     ImGui_ImplOpenGL3_Init(glsl_version);
 
-    // This function call won't return, and will engage in an infinite loop,
-    // processing events from the browser, and dispatching them.
-    emscripten_set_main_loop_arg(mainLoop, nullptr, 0, true);
+    emscripten_set_main_loop_arg(mainLoop, static_cast<void*>(&app), 0, true);
 }
 
 static auto mainLoop(void* arg) -> void
 {
-    // We can pass this argument as the second parameter of
-    // emscripten_set_main_loop_arg(), but we don't use that.
-    IM_UNUSED(arg);
+    auto* app   = static_cast<Application*>(arg);
+    auto& color = app->color;
 
-    // Our state (make them static = more or less global) as a convenience to
-    // keep the example terse.
-    static bool show_demo_window = true;
-    static ImVec4 color          = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-
-    SDL_Event event;
+    auto event = SDL_Event{};
     while (SDL_PollEvent(&event)) { ImGui_ImplSDL2_ProcessEvent(&event); }
 
-    // Start the Dear ImGui frame
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplSDL2_NewFrame();
     ImGui::NewFrame();
 
-    // 1. Show the big demo window (Most of the sample code is in
-    // ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear
-    // ImGui!).
-    if (show_demo_window) { ImGui::ShowDemoWindow(&show_demo_window); }
+    if (app->showDemoWindow) { ImGui::ShowDemoWindow(&app->showDemoWindow); }
 
-    // 2. Show a simple window that we create ourselves. We use a Begin/End pair
-    // to create a named window.
-    {
-        static float f     = 0.0f;
-        static int counter = 0;
+    ImGui::Begin("Hello, world!");
+    ImGui::Text("This is some useful text.");
+    ImGui::Checkbox("Demo Window", &app->showDemoWindow);
+    ImGui::ColorEdit3("clear color", (float*)&color);
 
-        // Create a window called "Hello, world!" and append into it.
-        ImGui::Begin("Hello, world!");
+    if (ImGui::Button("Button")) { app->counter++; }
+    ImGui::SameLine();
+    ImGui::Text("counter = %d", app->counter);
 
-        // Display some text (you can use a format strings too)
-        ImGui::Text("This is some useful text.");
+    auto const fps = ImGui::GetIO().Framerate;
+    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / fps, fps);
+    ImGui::End();
 
-        // Edit bools storing our window open/close state
-        ImGui::Checkbox("Demo Window", &show_demo_window);
-
-        // Edit 1 float using a slider from 0.0f to 1.0f
-        ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
-
-        // Edit 3 floats representing a color
-        ImGui::ColorEdit3("clear color", (float*)&color);
-
-        // Buttons return true when clicked (most widgets return true
-        // when edited/activated)
-        if (ImGui::Button("Button")) { counter++; }
-        ImGui::SameLine();
-        ImGui::Text("counter = %d", counter);
-
-        auto const fps = ImGui::GetIO().Framerate;
-        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / fps, fps);
-        ImGui::End();
-    }
-
-    // Rendering
     ImGuiIO& io = ImGui::GetIO();
-
     ImGui::Render();
-    SDL_GL_MakeCurrent(gWindow, gGraphicsContext);
+    SDL_GL_MakeCurrent(app->window, app->graphicsContext);
     glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
     glClearColor(color.x * color.w, color.y * color.w, color.z * color.w, color.w);
     glClear(GL_COLOR_BUFFER_BIT);
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-    SDL_GL_SwapWindow(gWindow);
+    SDL_GL_SwapWindow(app->window);
 }
